551394efa3b893a50b1b29684349205378c18e4b
dong <kjlk5589@naver.com>
2021-05-26 12:49:10 +0900

    2048 자바스크립트 구현1

--- 2048/2048.js
+++ 2048/2048.js
@@ -1,5 +1,150 @@
-window.addEventListener('keydown',abc)
+var board = Array(Array(0,0,0,0),Array(0,0,0,0),Array(0,0,0,0),Array(0,0,0,0));
+var tableID = Array(Array("00","01","02","03"),Array("10","11","12","13"),Array("20","21","22","23"),Array("30","31","32","33"));
+var score;
 
-function abc(e){
-    console.log(e)
+window.addEventListener("keydown",keyDownEventHandler)
+// 초기 설정
+init();
+function init(){
+    score=0;
+    for(var i=0;i<4;i++)
+        for(var j=0;j<4;j++)
+            board[i][j]=0;
+    for(var i=0;i<2;i++){
+        var rand = parseInt(Math.random()*16);
+        var y = parseInt(rand / 4);//0,1,2,3
+        var x = rand % 4;  //0,1,2,3
+        if(board[y][x]==0) board[y][x]=getNewNum();
+        else i--;
+    }
+    update();
 }
+
+// 게임 화면 업데이트
+function update(){
+    for(var i=0;i<4;i++){
+        for(var j=0;j<4;j++){
+            var cell = document.getElementById(tableID[i][j]);
+            cell.innerHTML = board[i][j]==0?"":board[i][j];
+            coloring(cell);
+        }
+    }
+    document.getElementById("score").innerHTML=score;
+}
+
+// 칸 색칠
+function coloring(cell){
+    var cellNum = parseInt(cell.innerHTML);
+    switch(cellNum){
+        case 0:
+        case 2:
+            cell.style.color="#684A23";
+            cell.style.background="#FBEDDC";
+            break;
+        case 4:
+            cell.style.color="#684A23";
+            cell.style.background="#F9E2C7";
+            break;
+        case 8:
+            cell.style.color="#684A23";
+            cell.style.background="#F6D5AB";
+            break;
+        case 16:
+            cell.style.color="#684A23";
+            cell.style.background="#F2C185";
+            break;
+        case 32:
+            cell.style.color="#684A23";
+            cell.style.background="#EFB46D";
+            break;
+        case 64:
+            cell.style.color="#FFFFFF";
+            cell.style.background="#EBA24A";
+            break;
+        case 128:
+            cell.style.color="#FFFFFF";
+            cell.style.background="#E78F24";
+            break;
+        case 256:
+            cell.style.color="#FFFFFF";
+            cell.style.background="#E87032";
+            break;
+        case 512:
+            cell.style.color="#FFFFFF";
+            cell.style.background="#E85532";
+            break;
+        case 1024:
+            cell.style.color="#FFFFFF";
+            cell.style.background="#E84532";
+            break;
+        case 2048:
+            cell.style.color="#FFFFFF";
+            cell.style.background="#E83232";
+            break;
+        default:
+            if(cellNum>2048){
+                cell.style.color="#FFFFFF";
+                cell.style.background="#E51A1A";
+            }
+            else{
+                cell.style.color="#684A23";
+                cell.style.background="#FBEDDC";
+            }
+            break;
+    }
+}
+// 숫자 생성 확률
+function getNewNum(){ //0~9
+    var rand = parseInt(Math.random()*10);
+    if(rand==0) return 4;
+    return 2;
+}
+
+
+function keyDownEventHandler(e){
+    switch(e.keyCode){
+        case 38: moveDir(0); break; //up
+        case 40: moveDir(1); break; //down
+        case 37: moveDir(2); break; //left
+        case 39: moveDir(3); break; //right
+    }
+}
+
+// 최대 점수 반환
+function getMaxNum(){
+    var ret=0;
+    for(var i=0;i<4;i++)
+        for(var j=0;j<4;j++)
+            if(board[i][j]>ret)
+                ret=board[i][j];
+    return ret;
+}
+
+
+// 보드판 이동 방향에 따른 회전 컨트롤
+function moveDir(opt){
+    switch(opt){
+        case 0: move(); break; //up
+        case 1: rotate(2); move(); rotate(2); break; //down
+        case 2: rotate(1); move(); rotate(3); break; //left
+        case 3: rotate(3); move(); rotate(1); break; //right
+    }
+    update();
+}
+
+
+// 보드판 회전
+function rotate(n){
+    while(n--){
+        var tempBoard = Array(Array(0,0,0,0),Array(0,0,0,0),Array(0,0,0,0),Array(0,0,0,0));
+        for(var i=0;i<4;i++)
+            for(var j=0;j<4;j++)
+                tempBoard[i][j]=board[i][j];
+        for(var i=0;i<4;i++)
+            for(var j=0;j<4;j++)
+                board[j][3-i]=tempBoard[i][j];
+
+    }
+}
+
+function move() { }
