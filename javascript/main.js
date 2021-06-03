e1c665b6aad18ecbea8708a4163ccb7830c18e0a
Hong-sk <sooyun9600@naver.com>
2021-06-03 21:58:44 +0900

    웹사이트 디자인 개선

--- javascript/main.js
+++ javascript/main.js
@@ -1,11 +1,23 @@
 const title = document.querySelector(".title");
-const space = document.getElementById("space");
-const div = "box";
+const buildings = document.getElementsByClassName("building");
+const footer = document.querySelector("footer");
+const cycle = document.querySelector("#cycle");
+const description = document.querySelector("#introduction");
+const cloud = document.querySelector("#cloud");
+const clouds = document.querySelector("#clouds");
+const sun = document.querySelector("#sun");
 
-title.addEventListener("click", fillDisplay);
+title.addEventListener("click", disapear);
 
-function fillDisplay() {
-  const box = document.createElement("div");
-  box.classList.add(div);
-  space.append(box);
+function disapear() {
+  for (i = 0; i < buildings.length; i++) {
+    buildings[i].classList.add("buildings-disapear");
+  }
+  footer.classList.add("footer-disapear");
+  cycle.classList.add("cycle-disapear");
+  title.classList.add("title-disapear");
+  description.classList.add("description-disapear");
+  cloud.classList.add("sky-disapear");
+  sun.classList.add("sky-disapear");
+  clouds.classList.add("clouds-disapear");
 }
