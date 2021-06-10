7f67a1ce2e236f656b067baea4c91fb034e08f85
Hong-sk <sooyun9600@naver.com>
2021-06-11 00:00:03 +0900

    자전거를 차로 변경해봄

--- javascript/main.js
+++ javascript/main.js
@@ -1,11 +1,12 @@
 const title = document.querySelector(".title");
 const buildings = document.getElementsByClassName("building");
 const footer = document.querySelector("footer");
-const cycle = document.querySelector("#cycle");
+const car = document.querySelector(".caricon");
 const description = document.querySelector("#introduction");
 const cloud = document.querySelector("#cloud");
 const clouds = document.querySelector("#clouds");
 const sun = document.querySelector("#sun");
+// const cycle = document.querySelector("#cycle");
 
 title.addEventListener("click", disapear);
 
@@ -14,10 +15,11 @@ function disapear() {
     buildings[i].classList.add("buildings-disapear");
   }
   footer.classList.add("footer-disapear");
-  cycle.classList.add("cycle-disapear");
   title.classList.add("title-disapear");
   description.classList.add("description-disapear");
   cloud.classList.add("sky-disapear");
   sun.classList.add("sky-disapear");
   clouds.classList.add("clouds-disapear");
+  car.classList.add("car-disapear");
+  // cycle.classList.add("cycle-disapear");
 }
