3f69f89832b947e16650591e98df8962d504dafe
Hong-sk <sooyun9600@naver.com>
2021-06-14 20:55:18 +0900

    bullsandcows bug fix and add function

--- javascript/bullsandcows.js
+++ javascript/bullsandcows.js
@@ -6,27 +6,18 @@ var form = document.querySelector("form");
 var input = document.querySelector("input");
 var button = document.querySelector("button");
 var answerList = document.querySelector(".answer-list")
-input.maxLength = 4;
 
 
 
-function randomNumber() {
-  numCandidates = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-  for (i = 0; i < 4; i++) {
-    var number = numCandidates.splice(Math.floor(Math.random() * (10 - i)), 1)[0];
-    numArray.push(number);
-  }
-}
-
-randomNumber();
-console.log(numArray);
 
 form.addEventListener("submit", function game(e) {
   e.preventDefault();
   var answer = input.value;
   console.log(answer);
-  if (answer == numArray.join("")) {
+  if (answer === numArray.join("")) {
     result.textContent = "🎉✨홈런🎉✨";
+    numArray = [];
+    cleanAnswerList();
     input.value = "";
     input.focus();
     randomNumber();
@@ -34,6 +25,7 @@ form.addEventListener("submit", function game(e) {
     var answerArray = answer.split("");
     var strike = 0;
     var ball = 0;
+    var out = 0;
     for (var i = 0; i < 4; i++) {
       //같은 자리인지 먼저 검사
       if (Number(answerArray[i]) === numArray[i]) {
@@ -42,23 +34,36 @@ form.addEventListener("submit", function game(e) {
       } else if (numArray.indexOf(Number(answerArray[i])) > -1) {
         ball++;
       }
+      else {
+        out++;
+      }
     }
     if (
-      numArray.indexOf(Number(answerArray[0])) &&
-      numArray.indexOf(Number(answerArray[1])) &&
-      numArray.indexOf(Number(answerArray[2])) &&
-      numArray.indexOf(Number(answerArray[3])) == -1
+      out === 4
     ) {
       result.textContent = "Out!";
       showScore();
       input.focus();
+      input.value = "";
     } else {
       result.textContent = strike + "S " + ball + "B";
       showScore();
       input.focus();
+      input.value = "";
     }
   }
 });
+
+
+function randomNumber() {
+  numCandidates = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  for (i = 0; i < 4; i++) {
+    var number = numCandidates.splice(Math.floor(Math.random() * (10 - i)), 1)[0];
+    numArray.push(number);
+  }
+  console.log(numArray);
+}
+randomNumber();
 
 function showScore() {
   var p = document.createElement("p");
@@ -73,3 +78,11 @@ function checkLength(input) {
     input.value = ""
   }
 }
+
+function cleanAnswerList() {
+  var preAnswers = document.querySelectorAll('.pre-answer');
+  for (i = 0; i < preAnswers.length; i++) {
+    answerList.removeChild(preAnswers[i]);
+  }
+}
+
